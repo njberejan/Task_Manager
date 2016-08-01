dee85d4c0403525138e0c0bc58ed87a8a4dc0a20
Nick Berejan <njberejan@gmail.com>
2016-08-01 15:15:27 -0400

    updated JS and responsiveness

---
+++ scripts/tasks-indexeddb.js
@@ -0,0 +1,25 @@
+storageEngine = function() new Promise(function(resolve, reject) {
+  return {
+    init : function(successCallback, errorCallback) {
+
+    },
+    initObjectStore : function(type, successCallback, errorCallback) {
+
+    },
+    save : function(type, successCallback, errorCallback) {
+
+    },
+    findAll : function(type, successCallback, errorCallback) {
+
+    },
+    delete : function(type, successCallback, errorCallback) {
+
+    },
+    findByProperty : function(type, successCallback, errorCallback) {
+
+    },
+    findById : function(type, successCallback, errorCallback) {
+
+    }
+  }
+}();
